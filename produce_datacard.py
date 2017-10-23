1a23a7d019ccb65ef2d0d603f13007727bafa630
Stefan Wunsch <stefan.wunsch@student.kit.edu>
2017-10-23 14:24:26 +0200

    blub

--- produce_datacard.py
+++ produce_datacard.py
@@ -11,16 +11,16 @@ logger = logging.getLogger("")
 
 
 def setup_logging(output_file, level=logging.DEBUG):
-    logger.setLevel(level)
-    formatter = logging.Formatter("%(name)s - %(levelname)s - %(message)s")
+        logger.setLevel(level)
+        formatter = logging.Formatter("%(name)s - %(levelname)s - %(message)s")
 
-    handler = logging.StreamHandler()
-    handler.setFormatter(formatter)
-    logger.addHandler(handler)
+        handler = logging.StreamHandler()
+        handler.setFormatter(formatter)
+        logger.addHandler(handler)
 
-    file_handler = logging.FileHandler(output_file, "w")
-    file_handler.setFormatter(formatter)
-    logger.addHandler(file_handler)
+        file_handler = logging.FileHandler(output_file, "w")
+        file_handler.setFormatter(formatter)
+        logger.addHandler(file_handler)
 
 
 def parse_arguments():
