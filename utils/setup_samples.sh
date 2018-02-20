519f23492254ae7ab8f752d5a1aefd8485dd5c91
Cristovao da Cruz e Silva <cbeiraod@cern.ch>
2018-02-20 20:06:59 +0000

    Updated sample locations for LIP

--- utils/setup_samples.sh
+++ utils/setup_samples.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 ARTUS_OUTPUTS=/storage/c/swozniewski/SM_Htautau/ntuples/Artus_2017-12-02/merged
+ARTUS_OUTPUTS=/lstore/cms/cbeiraod/HTT/Artus_2017-12-02/merged
 
-ARTUS_FRIENDS_ET=/storage/c/wunsch/Artus_2017-12-02/et_keras_4
-ARTUS_FRIENDS_MT=/storage/c/wunsch/Artus_2017-12-02/mt_keras_4
-ARTUS_FRIENDS_TT=/storage/c/wunsch/Artus_2017-12-02/tt_keras_4
+ARTUS_FRIENDS_ET=/lstore/cms/cbeiraod/HTT/Artus_2017-12-02/et_keras_4
+ARTUS_FRIENDS_MT=/lstore/cms/cbeiraod/HTT/Artus_2017-12-02/mt_keras_4
+ARTUS_FRIENDS_TT=/lstore/cms/cbeiraod/HTT/Artus_2017-12-02/tt_keras_4
 
 KAPPA_DATABASE=/storage/c/wunsch/kappa_database/datasets_2017-12-05.json
