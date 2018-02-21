fc5a392f672d2ba8aaa3f79276a9f52d2e7689e4
Cristovao da Cruz e Silva <cbeiraod@cern.ch>
2018-02-21 12:14:23 +0000

    Removed line that was left over by accident

--- utils/setup_samples.sh
+++ utils/setup_samples.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-ARTUS_OUTPUTS=/storage/c/swozniewski/SM_Htautau/ntuples/Artus_2017-12-02/merged
 ARTUS_OUTPUTS=/lstore/cms/cbeiraod/HTT/Artus_2017-12-02/merged
 
-ARTUS_FRIENDS_ET=/lstore/cms/cbeiraod/HTT/Artus_2017-12-02/et_keras_4
-ARTUS_FRIENDS_MT=/lstore/cms/cbeiraod/HTT/Artus_2017-12-02/mt_keras_4
-ARTUS_FRIENDS_TT=/lstore/cms/cbeiraod/HTT/Artus_2017-12-02/tt_keras_4
+ARTUS_FRIENDS_ET=/lstore/cms/cbeiraod/HTT/Artus_2017-12-02/et_keras_1
+ARTUS_FRIENDS_MT=/lstore/cms/cbeiraod/HTT/Artus_2017-12-02/mt_keras_1
+ARTUS_FRIENDS_TT=/lstore/cms/cbeiraod/HTT/Artus_2017-12-02/tt_keras_2
 
 KAPPA_DATABASE=/lstore/cms/cbeiraod/HTT/Artus_2017-12-02/datasets_2017-12-05.json
