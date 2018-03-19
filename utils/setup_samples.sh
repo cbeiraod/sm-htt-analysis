2828550500c2425d71862440e4402f9f9d991719
Cristovao da Cruz e Silva <cbeiraod@cern.ch>
2018-03-19 10:49:22 +0000

    Changed sample directories to point to samples trained at LIP

--- utils/setup_samples.sh
+++ utils/setup_samples.sh
@@ -2,8 +2,8 @@
 
 ARTUS_OUTPUTS=/lstore/cms/cbeiraod/HTT/Artus_2017-12-02/merged
 
-ARTUS_FRIENDS_ET=/lstore/cms/cbeiraod/HTT/Artus_2017-12-02/et_keras_1
-ARTUS_FRIENDS_MT=/lstore/cms/cbeiraod/HTT/Artus_2017-12-02/mt_keras_1
-ARTUS_FRIENDS_TT=/lstore/cms/cbeiraod/HTT/Artus_2017-12-02/tt_keras_2
+ARTUS_FRIENDS_ET=/lstore/cms/cbeiraod/HTT/Artus_2017-12-02/et_lip_1
+ARTUS_FRIENDS_MT=/lstore/cms/cbeiraod/HTT/Artus_2017-12-02/mt_lip_1
+ARTUS_FRIENDS_TT=/lstore/cms/cbeiraod/HTT/Artus_2017-12-02/tt_lip_1
 
 KAPPA_DATABASE=/lstore/cms/cbeiraod/HTT/Artus_2017-12-02/datasets_2017-12-05.json
